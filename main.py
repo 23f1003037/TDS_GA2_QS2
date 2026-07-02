1d753a565137913c86864b33eedf4e752c120a31
JonSnow <harshyadavbhu13@gmail.com>
2026-07-02 20:03:05 +0530

    Second

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import jwt
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -42,7 +43,7 @@ def verify(req: TokenRequest):
         }
 
     except jwt.PyJWTError:
-        raise HTTPException(
+        return JSONResponse(
             status_code=401,
-            detail={"valid": False}
+            content={"valid": False}
         )
